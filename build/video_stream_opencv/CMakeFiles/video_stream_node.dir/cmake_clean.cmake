882898ee29355b4ca3fc905f9af0a30318dd3ab1
salimbeni1 <etienne.salimbeni@gmail.com>
2021-07-16 18:26:14 +0200

    Camera feedback package added with ROS Communication

---
+++ build/video_stream_opencv/CMakeFiles/video_stream_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/video_stream_node.dir/src/video_stream_node.cpp.o"
+  "/media/xplore/etienne_vol/Xplore/HD_workspace/devel/lib/video_stream_opencv/video_stream.pdb"
+  "/media/xplore/etienne_vol/Xplore/HD_workspace/devel/lib/video_stream_opencv/video_stream"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/video_stream_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
